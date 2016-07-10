ddd7ebe3fcdbbe1af1bd333118d2409dc7034456
chooda <dmitriy@chudnyi.com>
2016-07-10 21:57:24 +0300

    ok2

--- assets/mobirise/js/script.js
+++ assets/mobirise/js/script.js
@@ -425,7 +425,7 @@
         var e = document.createElement("section");
         e.id = "top-1";
         e.className = "engine";
-        e.innerHTML = '<a href="https://mobirise.com">mobirise.com</a> Mobirise v2.11.1';
+        e.innerHTML = '<a href="https://mobirise.com">mobirise.com</a> Mobirise v2.14.2';
         document.body.insertBefore(e, document.body.childNodes[0]);
     }
 }();
